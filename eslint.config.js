fdcb2318814c0dc0239f03d8849978ce2318eb56
thonghp <thonghoangpham123@gmail.com>
2024-09-08 17:27:26 +0700

    add library for mui

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
   { ignores: ["dist"] },
-
   {
-    env: { browser: true, es2020: true, node: true },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
-      "plugin:react-hooks/recommended",
-    ],
     files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -41,27 +33,9 @@ export default [
         "warn",
         { allowConstantExport: true },
       ],
-
-      "react/prop-types": 0,
-      "react/display-name": 0,
-      "no-console": 1, // eslint nó sẽ kiểm tra đoạn code nào có console.log() thì báo xoá nó đi
+      "no-console": 1,
       "no-lonely-if": 1,
-      "no-unused-vars": 1, // biến không sử dụng
-      "no-trailing-spaces": 1, 
-      "no-multi-spaces": 1,
-      "no-multiple-empty-lines": 1, // thừa dòng
-      "space-before-blocks": ["error", "always"],
-      "object-curly-spacing": [1, "always"],
-      indent: ["warn", 2],
-      semi: [1, "never"],
-      quotes: ["error", "single"],
-      "array-bracket-spacing": 1,
-      "linebreak-style": 0,
-      "no-unexpected-multiline": "warn",
-      "keyword-spacing": 1,
-      "comma-dangle": 1,
-      "comma-spacing": 1,
-      "arrow-spacing": 1,
+      "no-unused-vars": 1,
     },
   },
 ];
