7413111c32374242a97c173d7083404a9969ae1d
thonghp <thonghoangpham123@gmail.com>
2024-09-09 15:21:38 +0700

    customize relative path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Typography from "@mui/material/Typography";
-import Board from "./pages/Boards/_id";
+import Board from "~/pages/Boards/_id";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme.js'
+import theme from '~/theme.js'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 
 
